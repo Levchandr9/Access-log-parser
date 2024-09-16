a2a7e5e43a125a32642bd2a2474db9eff4ef0f8f
Levchandr9 <levon.vagarshakovich9@gmail.com>
2024-09-16 21:45:02 +0300

    Добавлены арифимитеческие операции

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 
-import static java.lang.System.*;
-import static java.lang.System.out;
-
 public class Main {
     public static void main(String[] args) {
-        out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(in).nextLine();
-        System.out.println("Длина текстаа: " + text.length());
+        System.out.println("Введите первое число");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int addition = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        double multiplication = (double) firstNumber * secondNumber;
+        double division = (double) firstNumber / secondNumber;
+        double remainder = (double) firstNumber % secondNumber;
+        System.out.println(firstNumber + "+" + secondNumber + "=" + addition);
+        System.out.println(firstNumber + "-" + secondNumber + "=" + subtraction);
+        System.out.println(firstNumber + "*" + secondNumber + "=" + multiplication);
+        System.out.println(firstNumber + "/" + secondNumber + "=" + division);
+        System.out.println("Остаток от деления " +  firstNumber + " на " + secondNumber + " = " + remainder);
+
     }
 }
